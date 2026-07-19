634b8507f16afdeda96c19569ad66717f9ee34b1
Liv Johar <joharliv@gmail.com>
2026-07-19 22:52:40 +0530

    Time: 0 ms (100%), Space: 42.8 MB (52.49%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int freq[] = new int[26];
+        char res = 'a';
+        for(int i =0;i<s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+
+            if(freq[s.charAt(i) - 'a'] == 2){
+               res = s.charAt(i) ;
+               break;
+            }
+        }
+        return res;
+    }
+}
