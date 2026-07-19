6fce8a377db481717042cd5d352c50c74e3f2113
Liv Johar <joharliv@gmail.com>
2026-07-19 20:28:05 +0530

    Time: 3 ms (93.39%), Space: 46.5 MB (34.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char arr[] = s.toCharArray();
+        Arrays.sort(arr);
+
+        char arr2[] = t.toCharArray();
+        Arrays.sort(arr2);
+        
+       return Arrays.equals(arr, arr2);
+    }
+}
