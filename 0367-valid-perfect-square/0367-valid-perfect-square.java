407ae7255f8e4f545ecc70d1b21d8619f7c82171
Liv Johar <liv2545.beai24@chitkara.edu.in>
2026-07-28 23:12:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.6%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        long left = 1;
+        long right = num;
+
+        while(left<= right){
+            long mid = left + (right - left)/2;
+            long sq = mid*mid;
+            if(sq == num){
+                return true;
+            }
+            else if(sq > num){
+                right = mid -1;
+            }
+            else if(sq < num){
+                left = mid + 1;
+            }
+        }
+        return false; 
+    }
+}
