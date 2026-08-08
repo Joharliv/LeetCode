6e383fd3f07ba6ff4815238abf5fd6013ce57c34
Liv Johar <liv2545.beai24@chitkara.edu.in>
2026-08-08 20:20:58 +0530

    Time: 2 ms (98.01%), Space: 43.6 MB (52.84%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i : arr){
+            map.put(i , map.getOrDefault(i , 0)+1);
+        }
+
+        for(int freq : map.values()){
+            if(set.contains(freq)){
+              return false;
+            }
+            
+            set.add(freq);
+            
+        }
+        return true;
+    }
+}
