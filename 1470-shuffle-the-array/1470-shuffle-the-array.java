bef54ee27af3ade6c313be142440623dd215638a
Liv Johar <liv2545.beai24@chitkara.edu.in>
2026-08-13 15:48:51 +0530

    Time: 0 ms (100%), Space: 46.6 MB (22.82%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[nums.length];
+        int x = 0;
+        int y = n;
+        for(int i =0;i<nums.length;i+=2){
+            res[i] = nums[x];
+            res[i+1] = nums[y];
+
+            x++;
+            y++;
+        }
+        return res;
+    }
+}
