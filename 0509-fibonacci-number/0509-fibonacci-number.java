fd819e226beeb4775307a8bdcacfbda07a445ff4
Liv Johar <liv2545.beai24@chitkara.edu.in>
2026-09-03 14:32:55 +0530

    Time: 9 ms (42.69%), Space: 41.8 MB (85.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,7 @@
 class Solution {
     public int fib(int n) {
-        int f_zero = 0;
-        int f_one = 1;
-        int f_two = 0;
-        if(n == 0 || n == 1){
-            return n==0 ? f_zero : f_one;
-        }
-        for(int i = 2;i<=n;i++){
-            f_two = f_one + f_zero;
-            f_zero = f_one;
-            f_one = f_two;
-        }
-        return f_two;
+        if(n<= 1) return n;
+
+        return fib(n-1) + fib(n-2);
     }
 }
